ca7ae33c7e0918eeb91274c0e880a890800020d4
koyaaoyama <aoyama.ko0920@gmail.com>
2023-02-19 22:06:54 +0900

    implement BinarySearchTree class

--- src/dataStructures/binarySearchTree.ts
+++ src/dataStructures/binarySearchTree.ts
@@ -10,73 +10,105 @@ export class TreeNode {
   }
 }
 
-const insert = (node: TreeNode | null, value: number): TreeNode => {
-  if (node === null) return new TreeNode(value)
+class BinarySearchTree {
+  root: TreeNode | null
 
-  value < node.value
-    ? node.left = insert(node.left, value)
-    : node.right = insert(node.right, value)
-
-  return node
-}
-
-const inOrder = (node: TreeNode | null) => {
-  if (node === null) return
-
-  inOrder(node.left)
-  console.log(node.value)
-  inOrder(node.right)
-}
-
-const search = (node: TreeNode | null, value: number): boolean => {
-  if (node === null) return false
-
-  if (node.value === value) return true
-
-  return node.value > value ? search(node.left, value) : search(node.right, value)
-}
-
-const minValue = (node: TreeNode) => {
-  let currentNode = node
-  while (currentNode?.left) {
-    currentNode = currentNode.left
+  constructor(node?: TreeNode | null) {
+    this.root = node === undefined ? null : node
   }
 
-  return currentNode
-}
-
-const remove = (node: TreeNode | null, value: number): TreeNode | null => {
-  if (node === null) return node
-
-  if (value < node.value) {
-    node.left = remove(node.left, value)
-  } else if (value > node.value) {
-    node.right = remove(node.right, value)
-  } else {
-    if (node.left === null) {
-      return node.right
-    } else if (node.right === null) {
-      return node.left
+  insert(value: number) {
+    if (this.root === null) {
+      this.root = new TreeNode(value)
+      return
     }
 
-    let tmp = minValue(node.right)
-    node.value = tmp.value
-    node.right = remove(node.right, tmp.value)
+    const _insert = (node: TreeNode | null, value: number): TreeNode => {
+      if (node === null) return new TreeNode(value)
+
+      value < node.value
+        ? node.left = _insert(node.left, value)
+        : node.right = _insert(node.right, value)
+
+      return node
+    }
+
+    _insert(this.root, value)
   }
 
-  return node
+  inOrder() {
+    if (this.root === null) return
+
+    const _inOrder = (node: TreeNode | null) => {
+      if (node === null) return
+
+      _inOrder(node.left)
+      console.log(node.value)
+      _inOrder(node.right)
+    }
+
+    _inOrder(this.root)
+  }
+
+  search(value: number) {
+    if (this.root === null) return false
+
+    const _search = (node: TreeNode | null, value: number): boolean => {
+      if (node === null) return false
+
+      if (node.value === value) return true
+
+      return node.value > value
+        ? _search(node.left, value)
+        : _search(node.right, value)
+    }
+
+    return _search(this.root, value)
+  }
+
+  minValue(node: TreeNode) {
+    let currentNode = node
+    while (currentNode?.left) {
+      currentNode = currentNode.left
+    }
+
+    return currentNode
+  }
+
+  remove(value: number) {
+    const _remove = (node: TreeNode | null, value: number): TreeNode | null => {
+      if (node === null) return node
+
+      if (value < node.value) {
+        node.left = _remove(node.left, value)
+      } else if (value > node.value) {
+        node.right = _remove(node.right, value)
+      } else {
+        if (node.left === null) {
+          return node.right
+        } else if (node.right === null) {
+          return node.left
+        }
+
+        let tmp = this.minValue(node.right)
+        node.value = tmp.value
+        node.right = _remove(node.right, tmp.value)
+      }
+
+      return node
+    }
+
+    _remove(this.root, value)
+  }
 }
 
-let root = null
-root = insert(root, 3)
-root = insert(root, 6)
-root = insert(root, 5)
-root = insert(root, 7)
-root = insert(root, 1)
-root = insert(root, 10)
-root = insert(root, 2)
-inOrder(root)
-console.log('###############')
-root = remove(root, 6)
-inOrder(root)
-// console.log(search(root, 10))
+const binaryTree = new BinarySearchTree()
+binaryTree.insert(3)
+binaryTree.insert(6)
+binaryTree.insert(5)
+binaryTree.insert(7)
+binaryTree.insert(1)
+binaryTree.insert(10)
+binaryTree.insert(2)
+
+binaryTree.inOrder()
